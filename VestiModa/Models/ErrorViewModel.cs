29446f356db98bba782200806259396997ee7c32
FabioPenedo <fabiopenedo21@gmail.com>
2024-08-12 15:22:33 -0300

    primeiro commit

---
+++ VestiModa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VestiModa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
